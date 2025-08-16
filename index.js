3a5d44319988d7290348e646be807ec6aadefeb6
Zhomart Yerkin <zyerkin@gmail.com>
2025-08-17 02:13:54 +0500

    Added src with Welcome and random from lodash

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello from hexlet-js");
+import app from './src/my-application/index.js'
+
+import pkg from 'lodash';
+
+const { random } = pkg;
+
+app()
+
+console.log('Absolutely random number', random(0, 55));
